12b6145d83da10039a76179a4770e86be1fdf492
xingyue <qixingyue@126.com>
2015-11-30 16:12:35 +0800

    汇总文件大小

--- src/listfile.php
+++ src/listfile.php
@@ -6,8 +6,12 @@ include "config.php";
 $scs = new SCS(AccessKey, SecretKey);
 $bucketName = BUCKETNAME;
 $contents = $scs->getBucket($bucketName,PREFIX);
+$all_size = 0;
 
 foreach($contents as $file){
+	$all_size += $file['size'];
 	//var_dump($file);
 	echo str_pad($file['name'], 64 ,".") . $file['size'] .  "\n";
 }
+
+echo "All file size: " . $all_size . "\n";
