c800c831533f513ab00d27d1224187e508d72462
xingyue <qixingyue@126.com>
2016-02-15 12:19:45 +0800

    输出文件添加序号

--- src/listfile.php
+++ src/listfile.php
@@ -7,11 +7,12 @@ $scs = new SCS(AccessKey, SecretKey);
 $bucketName = BUCKETNAME;
 $contents = $scs->getBucket($bucketName,PREFIX);
 $all_size = 0;
+$index = 0;
 
-foreach($contents as $index=>$file){
+foreach($contents as $file){
 	$all_size += $file['size'];
 	//var_dump($file);
-	echo str_pad($index  . ". "  .  $file['name'], 64 ,".") . $file['size'] .  "\n";
+	echo str_pad($index++  . ". "  .  $file['name'], 64 ,".") . $file['size'] .  "\n";
 }
 
 echo "All file size: " . $all_size . "\n";
